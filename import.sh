606aabcdacefdaf91205e6282384d1ec70810682
vcoppe <vianney.coppe@gmail.com>
2026-03-12 13:17:53 +0100

    retry

--- import.sh
+++ import.sh
@@ -47,7 +47,7 @@ docker run \
     -v ./srtm:/tmp/srtm \
     -v ./logs:/graphhopper/logs \
     graphhopper \
-    -c "java -Xms50g -Xmx50g -XX:+UseParallelGC -Ddw.graphhopper.datareader.file=/data/data.osm.pbf -jar *.jar import /data/config.yml"
+    -c "java -Xms35g -Xmx35g -XX:+UseParallelGC -Ddw.graphhopper.datareader.file=/data/data.osm.pbf -jar *.jar import /data/config.yml"
 
 if [ ! -f logs/graphhopper.log ]; then
     echo "Failed to launch processing!"
